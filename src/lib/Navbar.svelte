f682ae413f74b16217bdc27935be2ce2229a0089
5upern1ce <alfietompsett@outlook.com>
2025-10-17 12:25:59 +0100

    Make carousel work on different pages :3

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<div class=" bg-gradient-to-r from-green-900 via-amber-400 via-green-300 to-orange-600">
+	<Navbar>
+		<NavBrand href="/">
+			<img src="/DB20_logo.jpg" class="me-3 h-10 align-middle sm:h-15" alt="DB2.0" />
+			<!-- <span class="self-center text-3xl font-extrabold whitespace-nowrap text-green-600 dark:text-green-900">DB</span>
+		<span class="inline-block self-center bg-gradient-to-t from-orange-800 via-orange-700 to-orange-300 bg-clip-text text-3xl font-extrabold whitespace-nowrap text-transparent">2.0</span> -->
+		</NavBrand>
+		<NavHamburger />
+		<NavUl class="">
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/docs/components/navbar">Navbar</NavLi>
+			<NavLi href="/pricing">Pricing</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import Navbar from '$lib/Navbar.svelte';
+	import Carousel from '$lib/Carousel.svelte';
 </script>
 
 <Navbar></Navbar>
+
+<Carousel></Carousel>
