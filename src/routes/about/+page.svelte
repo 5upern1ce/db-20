75fc1641afff2c25a40a916702c90704c3ff69fd
5upern1ce <alfietompsett@outlook.com>
2025-10-19 10:29:15 +0100

    fixed center

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,6 +3,13 @@
 	import Carousel from '$lib/Carousel.svelte';
 </script>
 
-<Navbar></Navbar>
-
-<Carousel></Carousel>
+<Navbar />
+<div class="bg-red-500">
+	<!-- full width row, center the inner fixed-width box -->
+	<div class="flex w-full justify-center pt-3">
+		<div class="w-[800px] max-w-full">
+			<!-- change 800px or use max-w-4xl -->
+			<Carousel />
+		</div>
+	</div>
+</div>
